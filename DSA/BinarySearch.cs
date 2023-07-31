808c9b8d3917ef081677485dc1752d4c6485c9bf
YUVANTHIKAS <107339334+YUVANTHIKAS@users.noreply.github.com>
2023-07-31 12:06:14 +0530

    BinarySearch.cs

--- DSA/BinarySearch.cs
+++ DSA/BinarySearch.cs
@@ -8,5 +8,27 @@ namespace DSA
 {
     internal class BinarySearch
     {
+        public void ReadTextFileAndSearch(string filePath)
+        {
+            string readAllText = File.ReadAllText(filePath);
+            string[] words = readAllText.Split(" ,");
+            Console.WriteLine("Enter key to search");
+            string key = Console.ReadLine();
+            bool isFind = false;
+            foreach (var data in words)
+            {
+                if (data.Equals(key))
+                {
+                    isFind = true;
+                }
+            }
+            if (isFind)
+            {
+                Console.WriteLine("Search element {0} found", key);
+            }
+            else
+                Console.WriteLine("Search element not found");
+
+        }
     }
 }
